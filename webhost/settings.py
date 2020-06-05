b468cfed4fde48fe08bdbebaf8e3b461ffcaaff8
vijaysardulgarh <vijaysardulgarh@gmail.com>
2020-06-05 12:43:26 +0530

    Project Updated

--- webhost/settings.py
+++ webhost/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django-heroku
 import os
 import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django.heroku.settings(locals())
