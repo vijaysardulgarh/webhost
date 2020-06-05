2c05cd4aa578a5e303bc079e1a66f2ef72c1e011
vijaysardulgarh <vijaysardulgarh@gmail.com>
2020-06-05 18:58:12 +0530

    update

--- webhost/urls.py
+++ webhost/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include(('blog.urls','blog'),namespace='blog'))
+    path('',include(('blog.urls','blog'),namespace='blog'))
 ]
 
